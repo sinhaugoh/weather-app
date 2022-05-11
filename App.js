4068993d0b982603e1fc9a81995789091e25cb1d
Goh Sin Hau <shgoh025@mymail.sim.edu.sg>
2022-05-11 15:44:49 +0800

    fetched current weather based on current location
    With the use of OpenWeatherMap API

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission denied");
+        return;
+      }
+
+      let loc = await Location.getCurrentPositionAsync({});
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${loc.coords.latitude}&lon=${loc.coords.longitude}&appid=e502ee0395a67375598a8180d06938c0`,
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((response) => response.json())
+        .then((json) => {
+          setLocation(json);
+        })
+        .catch((err) => console.log(err));
+    })();
+  }, []);
+
+  if (errorMsg !== null) {
+    // there is error
+    return (
+      <View style={styles.container}>
+        <Text>There's been an error: {errorMsg}</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  } else if (location !== null) {
+    // success
+    return (
+      <View style={styles.container}>
+        <Text>{JSON.stringify(location)}</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  } else {
+    // waiting (still waiting for gps to locate)
+    return (
+      <View style={styles.container}>
+        <Text>Waiting...</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
